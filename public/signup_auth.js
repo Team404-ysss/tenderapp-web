e8fef85230e0e31ebd28e1229ae5bc190fd519ff
SHRIRAM0509 <shriramgobu@gmail.com>
2019-03-23 11:20:33 +0530

    firebase auth

---
+++ public/signup_auth.js
@@ -0,0 +1,26 @@
+ (function(){
+	// initialising firebase
+	const config = {
+	    apiKey: "AIzaSyAhMIyNFN31gW9YYHxTvz3T5FSJ4Sx1Uek",
+	    authDomain: "tender-261f1.firebaseapp.com",
+	    databaseURL: "https://tender-261f1.firebaseio.com",
+	    projectId: "tender-261f1",
+	    storageBucket: "tender-261f1.appspot.com",
+	    messagingSenderId: "175461595113"
+	};
+	firebase.initializeApp(config);
+
+	// getting elements from a form
+	const email = document.getElementById('input-email');
+	const pwd = document.getElementById('input-password');
+	const submit = document.getElementById('btn1');
+
+	//sign up
+	submit.addEventListener('click', e =>{
+		const email = email.value;
+		const pass = pwd.value;
+		const auth  = firebase.auth();
+		auth.createUserWithEmailAndPassword(email,pass);
+		promise.catch(e => console.log(e.message));
+	});
+}());
